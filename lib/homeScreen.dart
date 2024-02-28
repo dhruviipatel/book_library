b621172d94b76b62852bd3663e036de24d921a1a
Dhruvi <dhruvi244210@gmail.com>
2024-02-28 18:39:12 +0530

    design done

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,5 +1,7 @@
+import 'package:book_library/appColors.dart';
 import 'package:book_library/myController.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -72,8 +74,8 @@ class HomeScreen extends StatelessWidget {
                             physics: const NeverScrollableScrollPhysics(),
                             gridDelegate:
                                 const SliverGridDelegateWithFixedCrossAxisCount(
-                                    mainAxisSpacing: 10,
-                                    mainAxisExtent: 300,
+                                    mainAxisSpacing: 25,
+                                    mainAxisExtent: 310,
                                     crossAxisSpacing: 10,
                                     crossAxisCount: 3),
                             itemCount: controller.bookList.length,
@@ -83,6 +85,7 @@ class HomeScreen extends StatelessWidget {
                               return Container(
                                 //color: Colors.grey,
                                 child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Container(
                                       height: 150,
@@ -94,11 +97,45 @@ class HomeScreen extends StatelessWidget {
                                               ),
                                               fit: BoxFit.cover)),
                                     ),
-                                    Text(
-                                      '${book['title']}',
-                                      style: const TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            '${book['title']}',
+                                            style: const TextStyle(
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text('${book['author_names']}',
+                                              style: const TextStyle(
+                                                color: AppColors.primaryColor,
+                                                fontSize: 11,
+                                              )),
+                                          Text('${book['first_publish_year']}',
+                                              style: const TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 11,
+                                              )),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Container(
+                                      height: 40,
+                                      color: Colors.green,
+                                      child: const Center(
+                                        child: Text(
+                                          "Read",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
                                     )
                                   ],
                                 ),
